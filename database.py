eefda13434868f8cc9e6afe088272e9549440b97
aboud20-meet <aboud20@meet.mit.edu>
2019-01-17 19:58:21 +0200

    work more

--- database.py
+++ database.py
@@ -7,6 +7,17 @@ engine = create_engine('sqlite:///database.db')
 Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+Base = declarative_base()
 
 def function(parameter):
     pass
+
+class song(Base):
+	__tablename__ = 'songs'
+    	id = Column(Integer, primary_key = True) 
+    	name = Column(String)
+		artist = Column(String)
+		album = Column(String)
+		year = Column(Integer)	 
+
+def add_student(name, year, finished_lab):
